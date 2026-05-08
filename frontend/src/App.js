da95ca6453a7bfe0593757d059c67eabf1c74c5d
Habeebullah Fiaz <f.z010174@gmail.com>
2026-05-08 08:45:58 +0500

    Update API URL for production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://weather-project-seven-jet.vercel.app";
 
 const weatherIcons = {
   Clear: "☀️", Clouds: "☁️", Rain: "🌧️",
